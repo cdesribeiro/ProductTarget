294229320c13fea45302119aa0d42c840d303d7f
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2021-12-07 06:10:39 -0300

    add libs - UI

--- TargetProduct.UI/Startup.cs
+++ TargetProduct.UI/Startup.cs
@@ -22,7 +22,7 @@ namespace TargetProduct.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
